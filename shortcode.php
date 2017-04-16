cfd7a6d15046b8ef70fea7741ccda3c504bdbbf7
xc-racer99 <xc-racer2@live.ca>
2017-04-15 18:42:59 -0700

    Add leaflet-omnivore plugin

--- shortcode.php
+++ shortcode.php
@@ -11,11 +11,13 @@ function lhgr_shortcode_init()
 		$content .= <<<EOD
 <script>
 function initializeMap() {
-var mymap = L.map('lhgr_map').setView([51.505, -0.09], 13);
-var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mymap);
+	var mymap = L.map('lhgr_map').setView([50.745711, -119.136533], 13);
+	var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(mymap);
+
+	omnivore.gpx('https://xc-racer2.duckdns.org/plugins/wp-content/uploads/2017/04/alder_lane.gpx').addTo(mymap);
 }
 </script>
 EOD;
@@ -33,7 +35,11 @@ function lhgr_register_resources()
 	wp_register_script('leaflet-base-js', '//unpkg.com/leaflet@1.0.3/dist/leaflet.js', null, '1.0.3');
 	wp_register_style('leaflet-base-css', '//unpkg.com/leaflet@1.0.3/dist/leaflet.css', null, '1.0.3');
 
+
+	// Leaflet omnivore
+	wp_register_script('leaflet-omnivore', '//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.3.1/leaflet-omnivore.min.js', array('leaflet-base-js'), '0.3.1');
+
 	// Helper JS file to delay initializing the map until its ready
-	wp_register_script('lhgr_leaflet_helper', plugins_url('initializeMap.js', __FILE__), array('leaflet-base-js'));
+	wp_register_script('lhgr_leaflet_helper', plugins_url('initializeMap.js', __FILE__), array('leaflet-base-js', 'leaflet-omnivore'));
 }
 add_action( 'wp_enqueue_scripts', 'lhgr_register_resources');
